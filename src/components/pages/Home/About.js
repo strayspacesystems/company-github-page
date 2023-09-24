243128cddf6864f21030d04e4eb064bbdd784314
icedragon789 <icedragon072@gmail.com>
2023-09-24 15:39:05 -0400

    Edited about us text

--- src/components/pages/Home/About.js
+++ src/components/pages/Home/About.js
@@ -15,26 +15,10 @@ function About() {
                 <div class="row content">
                     <div class="col-lg-6">
                         <p>
-                        StraySpaceSystems LLC was founded by University of New Haven graduates 
-                        Benjamin Greenfield, Steven Atilho, and Benjamin Placzek on Jun 27, 2022. 
-                        The idea for an intelligent parking solution was born amidst difficulties 
-                        with campus parking. After one too many walks up the steep hill up to campus 
-                        because they were not able to find parking, the team decided that enough was 
-                        enough and the idea for StraySpaceSystems was born, under its original name 
-                        “SmartPark”. “SmartPark” started off as a capstone project that was able to 
-                        report parking availability and make basic parking predictions. With much 
-                        thanks to the support from professors and campus staff alike, the project 
-                        was able to win many awards at regional conferences and even the University 
-                        of New Haven engineering expo. It was also the topic of a research paper 
-                        welcomed by the 11th International Conference on Digital Image Processing 
-                        and Vision (ICPIV 2022)
+                        After failed attempts to find nearby parking and strenuous walks up steep hills, the idea for an intelligent parking solution was developed between three university students who were tired of wasting time on losing potential parking spots. 
                         </p>
                         <p>
-                        After graduation, David Leandres and Drew Dorris were eager to join 
-                        StraySpaceSystems LLC, providing expertise in business management and 
-                        web development, respectively. Since May 2022, the group has been hard 
-                        at work to program an intelligent parking solution and solidify our status 
-                        as an official LLC in the State of Connecticut. 
+                        Starting off as a capstone project on reporting parking availability, StraySpaceSystems LLC was created. With support from professors, campus staff, and friends,  the project has won awards at regional conferences alongside the University of New Haven Engineering Expo. StraySpaceSystems LLC was also the topic of the research paper welcomed by the 11th International Conference on Digital Image Processing and Vision (ICPIV 2022). 
                         </p>
                     </div>
                     <div class="col-lg-2"></div>
